b380851f3778b2ea674e3c8f2026ec45c819e817
Jorge Velarde <jorge.velarde@thermofisher.com>
2019-02-01 10:29:57 -0800

    Solved chap01ex.py

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -14,13 +14,66 @@ import nsfg
 import thinkstats2
 
 
+def ReadFemResp(dct_file='2002FemResp.dct',
+                dat_file='2002FemResp.dat.gz',
+                nrows=None):
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip', nrows=nrows)
+    CleanFemResp(df)
+    return df
+
+
+# def ReadFemResp(dct_file='2002FemResp.dct',
+#                 dat_file='2002FemResp.dat.gz',
+#                 nrows=None):
+#     """Reads the NSFG respondent data.
+#
+#     dct_file: string file name
+#     dat_file: string file name
+#
+#     returns: DataFrame
+#     """
+#     dct = thinkstats2.ReadStataDct(dct_file)
+#     df = dct.ReadFixedWidth(dat_file, compression='gzip', nrows=nrows)
+#     CleanFemResp(df)
+#     return df
+
+
+def CleanFemResp(df):
+    pass
+
+
+def ValidatePregnum(resp):
+    preg = nsfg.ReadFemPreg()
+    preg_map = nsfg.MakePregMap(preg)
+
+    for index, pregnum in resp.pregnum.items():
+        caseid = resp.caseid[index]
+        indexes = preg_map[caseid]
+
+        if len(indexes) != pregnum:
+            print(caseid, len(indexes), pregnum)
+            return False
+
+    return True
+
+
 def main(script):
     """Tests the functions in this module.
 
     script: string script name
     """
-    print('%s: All tests passed.' % script)
+    # resp = ReadFemResp()
+    # print(resp.pregnum.value_counts()[1])
+    #
+    # print('%s: All tests passed.' % script)
 
+    resp = ReadFemResp()
+
+    assert (len(resp) == 7643)
+    assert (resp.pregnum.value_counts()[1] == 1267)
+    assert (ValidatePregnum(resp))
+    print('%s: All tests passed.' % script)
 
 if __name__ == '__main__':
     main(*sys.argv)

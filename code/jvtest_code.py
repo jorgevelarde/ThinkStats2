95d1280f6eff53a50f9e7127134ba5c9874c2f0c
Jorge Velarde <jorge.velarde@thermofisher.com>
2019-02-07 17:48:12 -0800

    Added test code and solved chap02ex.ipynb

--- code/jvtest_code.py
+++ code/jvtest_code.py
@@ -1,3 +1,4 @@
+# ch1
 # import nsfg
 #
 # df = nsfg.ReadFemPreg()
@@ -11,6 +12,7 @@
 #         h += 1
 #
 # print h
+import math
 
 import thinkstats2
 import thinkplot
@@ -21,7 +23,8 @@ thinkplot.Hist(hist)
 
 preg = nsfg.ReadFemPreg()
 live = preg[preg.outcome == 1]
-hist = thinkstats2.Hist(live.birthwgt_lb, label="birthwgt_lb")
+
+# hist = thinkstats2.Hist(live.birthwgt_lb, label="birthwgt_lb")
 # thinkplot.Hist(hist)
 # thinkplot.Show(xlabel='pounds', ylabel='frequency')
 # thinkplot.Hist(hist)
@@ -39,15 +42,42 @@ hist = thinkstats2.Hist(live.birthwgt_lb, label="birthwgt_lb")
 
 firsts = live[live.birthord == 1]
 others = live[live.birthord != 1]
-first_hist = thinkstats2.Hist(firsts.prglngth)
-other_hist = thinkstats2.Hist(others.prglngth)
-
-width = 0.45
-thinkplot.PrePlot(2)
-
-# hist = thinkstats2.Hist(live.prglngth, label="preglen")
-# first_hist = thinkstats2.Hist(firsts.prglngth)
 
 
-thinkplot.Show(xlabel='weeks', ylabel='frequency', xlim=[27, 46])
-thinkplot.PrePlot(2)
+def printFirstVsOthersHistograms():
+    first_hist = thinkstats2.Hist(firsts.prglngth)
+    other_hist = thinkstats2.Hist(others.prglngth)
+
+    width = 0.45
+    thinkplot.PrePlot(2)
+    thinkplot.Hist(first_hist, align='right', width=width, label='first')
+    thinkplot.Hist(other_hist, align='left', width=width, label='others')
+
+    thinkplot.Show(xlabel='weeks', ylabel='frequency', xlim=[27, 46])
+
+
+def printDistributionSummary(series):
+    mean = series.mean()
+    var  = series.var()
+    std  = series.std()
+    print(mean, var, std)
+
+
+def CohenEffectSize(group1, group2):
+    diff = group1.mean() - group2.mean()
+    var1 = group1.var()
+    var2 = group2.var()
+    n1, n2 = len(group1), len(group2)
+    pooled_var = (n1 * var1 + n2 * var2) / (n1 + n2)
+    d = diff / math.sqrt(pooled_var)
+    return d
+
+
+# printFirstVsOthersHistograms()
+printDistributionSummary(live.prglngth)
+printDistributionSummary(firsts.prglngth)
+printDistributionSummary(others.prglngth)
+
+print(CohenEffectSize(firsts.prglngth, others.prglngth))
+
+
